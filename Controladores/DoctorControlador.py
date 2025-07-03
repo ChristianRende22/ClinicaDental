2828e06bc2e44f015a37dc620df855391bd4aacc
Merl024 <129928988+Merl024@users.noreply.github.com>
2025-07-03 15:24:33 -0600

    Update DoctorControlador.py

--- Controladores/DoctorControlador.py
+++ Controladores/DoctorControlador.py
@@ -1,3 +1,4 @@
+# Importaciones necesarias para no tener porblema con los path o importaciones de clase
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
