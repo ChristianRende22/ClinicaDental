dc5eefb966a89f94219650f91bf596bd99b17c55
Merl024 <129928988+Merl024@users.noreply.github.com>
2025-07-03 15:23:48 -0600

    Update DoctorControlador.py

--- Controladores/DoctorControlador.py
+++ Controladores/DoctorControlador.py
@@ -2,11 +2,6 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# from Controladores.CitaControlador import Cita
-# from Vistas.VistaDoctor import AgregarHorarioDialog
-
-# from Modelos.Doctor import Doctor
-
 from PyQt6.QtWidgets import QDialog, QMessageBox, QInputDialog, QComboBox, QTimeEdit, QVBoxLayout, QFormLayout, QDialogButtonBox
 
 from PyQt6.QtCore import QTime
@@ -16,6 +11,8 @@ from typing import List
 import re
 
 from datetime import datetime
+
+from Modelos.DoctorModelo import Doctor
 
 class Horario:
     """
@@ -144,32 +141,6 @@ class AgregarHorarioDialog(QDialog):
             'hora_inicio': self.hora_inicio_edit.time().toString("HH:mm"),
             'hora_fin': self.hora_fin_edit.time().toString("HH:mm")
         }
-
-    
-class Doctor:
-    def __init__(self, nombre, apellido, dui, especialidad, telefono, correo):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.dui = dui
-        self.especialidad = especialidad
-        self.telefono = telefono
-        self.correo = correo
-        self.citas = [] 
-        self.horario = []
-
-    def __str__(self):
-        return f"{self.nombre} {self.apellido} ({self.especialidad})"
-
-    def mostrar_citas(self, resultado_text):
-        """Muestra todas las citas asociadas a este doctor en el QTextEdit resultado_text."""
-        if not self.citas:
-            resultado_text.append(f"No hay citas registradas para el Dr. {self.nombre} {self.apellido}.")
-            return
-        resultado_text.append(f"Citas del Dr. {self.nombre} {self.apellido}:")
-        for cita in self.citas:
-            resultado_text.append(str(cita))
-        resultado_text.append("")
-
 
 class ControladorDoctor:
     def __init__(self, vista):
