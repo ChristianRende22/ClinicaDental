5cb206402b40d524b7e4afb375943eed546a7f44
Christian <codirrl@gmail.com>
2025-07-03 19:37:14 -0600

    Cambio paciente
    Conexion mejorada con los archivos, Paso de la clase paciente con sus metodos

--- Controladores/PacienteControlador.py
+++ Controladores/PacienteControlador.py
@@ -3,9 +3,10 @@ import sys
 import os 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from Vistas.DoctorVista import Doctor
+from Modelos.PacienteModelo import Paciente
+from Controladores.DoctorControlador import Doctor
 from Controladores.CitaControlador import Cita
-from Vistas.TratamientoVista import Tratamiento
+from Controladores.TratamientoControlador import Tratamiento
 from datetime import datetime
 from typing import List
 import re
@@ -13,44 +14,6 @@ import re
 
 
 
-
-class Paciente:
-    """Clase que representa un paciente de la clínica"""
-    def __init__(self, nombre: str, apellido: str, fecha_nacimiento: datetime, dui: str, 
-                 telefono: int, correo: str, saldo_pendiente: float = 0.0):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.fecha_nacimiento = fecha_nacimiento  # Se puede cambiar a fecha_nacimiento en el futuro
-        self.dui = dui
-        self.telefono = telefono
-        self.correo = correo
-        self.saldo_pendiente = saldo_pendiente
-        self.historial_medico: List[Tratamiento] = []
-        self.citas: List[Cita] = []
-        self.fecha_registro = datetime.now().strftime('%d/%m/%Y - %H:%M:%S')
-    
-    def agregar_tratamiento(self, tratamiento: Tratamiento):
-        """Agrega un tratamiento al historial médico del paciente"""
-        self.historial_medico.append(tratamiento)
-    
-    def agregar_cita(self, cita: Cita):
-        """Agrega una cita al paciente"""
-        self.citas.append(cita)
-    
-    def calcular_total_tratamientos(self) -> float:
-        """Calcula el costo total de todos los tratamientos"""
-        return sum(tratamiento.costo for tratamiento in self.historial_medico)
-    
-    def calcular_total_citas(self) -> float:
-        """Calcula el costo total de todas las citas"""
-        return sum(cita.costo_cita for cita in self.citas)
-    
-    def get_balance_total(self) -> float:
-        """Calcula el balance total del paciente"""
-        return self.calcular_total_tratamientos() + self.calcular_total_citas() + self.saldo_pendiente
-    
-    def __str__(self):
-        return f"Paciente: {self.nombre} {self.apellido} - DUI: {self.dui}"
 
 class PacienteControlador:
     """Controlador para manejar la lógica de negocio de los pacientes"""
